016f7622dc8827a1096ade7de8d2015217c08507
Rithvik-padma <rithvik.padma@gmail.com>
2023-10-22 22:53:11 +0530

    chore: starter template

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      'tab': {'max': '900px'},
+      'mob': {'max': '450px'},
+    }
   },
   plugins: [],
 }
